38612e2b1c6aba6444161d0ba0d201322ae0fe6f
Bruno Rangel <113265945+BruRangel@users.noreply.github.com>
2024-08-05 18:57:55 -0300

    1

---
+++ AED/abp_percursos_2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+typedef int TipoChave;
+
+typedef struct No {
+    TipoChave chave;
+    struct No *esq, *dir;
+} No;
+
+typedef No *Pont;
+
